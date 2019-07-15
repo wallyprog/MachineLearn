7042824011a0e2a6e922a8c1bf1caab824e7c5cb
Wallyson <wallyson@aluno.fapce.edu.br>
2019-07-15 14:58:46 -0300

    classes base

---
+++ POO/main.py
@@ -0,0 +1,6 @@
+from robos import *
+
+class Main(object):
+
+    r1 = Robos(5,5)
+    print (r1.x)
---
+++ POO/robos.py
@@ -0,0 +1,32 @@
+class Robos:
+    mensagem_erro = 'Valor não pode ser aplicado'
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def move_up(self):
+        if ( self.y < 10):
+            self.y += 1
+        else:
+            print (self.mensagem_erro)
+    
+    def move_down(self):
+        if ( self.y > 0):
+            self.y -= 1
+        else:
+            print (self.mensagem_erro)
+
+    def move_right(self):
+        if ( self.x < 10):
+            self.x += 1
+        else:
+            print (self.mensagem_erro)
+
+    def move_left(self):
+        if ( self.x > 0):
+            self.y -= 1
+        else:
+            print (self.mensagem_erro)
+    
+
+    
